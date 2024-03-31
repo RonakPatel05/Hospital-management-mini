5c1eb75d8343446fb008f0b27d91e468969b65de
Ronak Patel <ronak.wcity@gmail.com>
2024-03-31 22:44:52 +0530

    first commit

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = () => {
   mongoose
-    .connect(process.env.MONGO_URI, {
-      dbName: "MERN_STACK_HOSPITAL_MANAGEMENT_SYSTEM",
-    })
+    .connect('mongodb://localhost:27017/Hospital')
     .then(() => {
       console.log("Connected to database!");
     })
